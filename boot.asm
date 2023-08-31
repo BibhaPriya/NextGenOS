86aefd4d875620a523feb265bfac7d5da3a6f8db
BibhaPriya <bibhapri.35@gmail.com>
2023-09-01 05:16:00 +0530

    added User defined interupt, reading from harddisk beyond the bbot sector

--- boot.asm
+++ boot.asm
@@ -11,6 +11,20 @@ _start:
 
 start:
     jmp 0x7c0:step2
+
+handle_zero:
+    mov ah,0x0e
+    mov al, 'A'
+    mov bx, 0x0
+    int 0x10
+    iret
+handle_one:
+    mov ah, 0x0e
+    mov al, 'a'
+    mov bx, 0x00
+    int 0x10
+    iret
+
 step2:
     cli
     mov ax, 0x7c0
@@ -20,8 +34,28 @@ step2:
     mov ss, ax
     mov sp, 0x7c00
     sti
-
+    mov word[ss:0x00], handle_zero
+    mov word[ss:0x02], 0x7c0
+    int 0x0
+    mov word[ss:0x04], handle_one
+    mov word[ss:0x06], 0x7c0
+    int 1 
     mov si, message
+    call print
+    mov ah, 0x02
+    mov al, 0x01
+    mov ch, 0x00
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer
+    int 0x13
+    jc error
+    mov si, buffer
+    call print
+    jmp $
+
+error:
+    mov si, err_msg
     call print
     jmp $
 print:
@@ -39,7 +73,8 @@ print_char:
     mov ah, 0x0e
     int 0x10
     ret
-
+err_msg: db "Failed to load Message"
 message: db "Hello World", 0
 times 510 - ($-$$) db 0
 dw 0xaa55
+buffer:
